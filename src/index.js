612bf71b0748cf5fce375a9dadbbd3715f7ed75d
DBenson1017 <devinbenson@gmail.com>
2020-08-25 14:52:46 -0400

    lab completed sans extra deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+function addTask(){
+  let input = document.querySelector('input').value;
+  let list = document.getElementById('tasks');
+  let task = document.createElement('li');
+  task.innerText = input;
+  list.append(task);
+};
+
+let button = document.querySelectorAll('input')[1]
+
+button.addEventListener('click', function(e){
+  e.preventDefault();
+  addTask();
+});
+
 });
